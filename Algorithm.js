f99cb7ee4ea413ee8b3d85ee083a7baf94638d39
Damian Kokot <kokocik1213@gmail.com>
2019-07-23 13:48:49 +0200

    Added simple algorithms

---
+++ Algorithm.js
@@ -0,0 +1,24 @@
+﻿var data;
+function getStopId(name) {
+    console.log(name);
+    return data.nodes.filter((item) => {
+        return item.stop_name == name;
+    })[0].id;
+}
+function getResponse(sourceName, targetName) {
+    console.log("source " + getStopId(sourceName));
+    console.log("target " + getStopId(targetName));
+}
+
+
+module.exports = {
+    GetResponse: getResponse,
+    getData: function () {
+        return data;
+    },
+    setData: function (Data) {
+        data = Data;
+    }
+
+};
+
